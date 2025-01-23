2dc149be05baa1c1521a9dbb8c519bc96ba9b290
singvir23 <viraajsingh135@gmail.com>
2025-01-23 14:33:48 -0800

    stuff wasn't being used, so I removed it

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect} from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
